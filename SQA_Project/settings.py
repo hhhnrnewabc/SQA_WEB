a36a9ab92cf0c190048a033ce1350e52fc317517
YuYan <hhhnrnew82@gmail.com>
2014-05-21 13:02:32 +0800

    Add Django Debug Toolbar

--- SQA_Project/settings.py
+++ SQA_Project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',  # Django Debug Toolbar
     'rest_framework',
     'rest_framework.authtoken',
     'baseuser',
