3928babef302d2cc33df831d4026da96e8b0f4da
YuYan <hhhnrnew82@gmail.com>
2014-05-31 15:00:52 +0800

    coveralls.io

--- manage.py
+++ manage.py
@@ -14,14 +14,6 @@ else:
 
 if __name__ == "__main__":
 
-    with open(AWSTMPFILE, 'w') as outfile:
-
-        if sys.argv[1] == "travis-ci":
-            outfile.write("False")
-            sys.argv = [sys.argv[0]] + sys.argv[2:]
-        else:
-            outfile.write("True")
-
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SQA_Project.settings")
 
     from django.core.management import execute_from_command_line
--- runtests.py
+++ runtests.py
@@ -1,12 +1,18 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import os
 import sys
 
+from manage import AWSTMPFILE
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SQA_Project.settings")
 
 if __name__ == "__main__":
+
+    with open(AWSTMPFILE, 'w') as outfile:
+        outfile.write("False")
+
     from django.core.management import execute_from_command_line
     args = sys.argv
     args.insert(1, "test")
