e5c2299180778fbe56d0e8788ca378be46b2d43e
YuYan <hhhnrnew82@gmail.com>
2014-06-05 19:02:13 +0800

    Modify game model

--- game_info/models.py
+++ game_info/models.py
@@ -1,34 +1,34 @@
 from django.db import models
-from steam.models import Game
+from steam_dev.models import SteamDevAPPS
 
 
 class GameRelation(models.Model):
 
-    game = models.IntegerField(primary_key=True)
+    game = models.ForeignKey(SteamDevAPPS, primary_key=True)
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
-    def update_game(self, game, commit=True, *args, **kwargs):
-        try:
-            m = GameRelation.objects.get(game=game.id)
-            m.game = game.id
-            m.name = game.name
-            m.save()
-        except GameRelation.DoesNotExist:
-            pass
+    # def update_game(self, game, commit=True, *args, **kwargs):
+    #     try:
+    #         m = GameRelation.objects.get(game=game.id)
+    #         m.game = game.id
+    #         m.name = game.app_name
+    #         m.save()
+    #     except GameRelation.DoesNotExist:
+    #         pass
 
-    def update_games(self, games, commit=True, *args, **kwargs):
-        for g in games:
-            try:
-                m = GameRelation.objects.get(game=g.id)
-                if m.name != g.name:
-                    m.name = g.name
-                    m.save()
-            except GameRelation.DoesNotExist:
-                m = GameRelation.objects.create(name=g.name, game=g.id)
-                m.save()
+    # def update_games(self, games, commit=True, *args, **kwargs):
+    #     for g in games:
+    #         try:
+    #             m = GameRelation.objects.get(game=g.id)
+    #             if m.name != g.name:
+    #                 m.name = g.name
+    #                 m.save()
+    #         except GameRelation.DoesNotExist:
+    #             m = GameRelation.objects.create(name=g.app_name, game=g.id)
+    #             m.save()
 
 
 class GameInfo(models.Model):
